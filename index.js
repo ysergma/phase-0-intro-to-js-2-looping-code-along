d7f541fcc062712a427012562b703c5283c5023b
Youssouf Sergma <sil3nthilldz@gmail.com>
2023-07-26 22:11:35 +0100

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+
 // Code your solutions in this file
+
+const writeCards=function(name){
+
+    const newArr=[];
+    for(let i=0;i<name.length;i++){
+       const mssges= `Thank you, ${name[i]}, for the wonderful surprise gift!`
+        newArr.push(mssges) 
+    }
+    return newArr
+    
+}
+
+const countDown=function(integer){
+    let i=0;
+    while (i<=integer) {
+        console.log(integer)
+
+         --integer
+      }
+
+}
